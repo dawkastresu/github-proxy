ddccd25803e61feb6de91d1a53741fb4d09601e2
Dawid <dawidmalczewski3@gmail.com>
2025-06-03 19:44:41 +0200

    Initialize commit

---
+++ src/main/java/com/dawkastresu/git_proxy/GitProxyApplication.java
@@ -0,0 +1,13 @@
+package com.dawkastresu.git_proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitProxyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dawkastresu/git_proxy/GitProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dawkastresu.git_proxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
